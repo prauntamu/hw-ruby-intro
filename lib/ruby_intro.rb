9931a47783eaec5aba09e9f1d9e497a5f05741ea
prauntamu <58442845+prauntamu@users.noreply.github.com>
2020-04-23 15:58:13 -0500

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,35 +1,70 @@
-# When done, submit this entire file to the autograder.
-
+#Hw 1 for csce 431 
+#Paige Raun 
+#done in class in singapore 
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+result = 0
+arr.each{|x| result = result +x}
+result
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  arr.max(2).sum
+
+# result =0
+  #  # arr.sort!
+  #   # if arr.length ==1
+  #    #     return arr[0]
+  #     # else if arr.length > 1
+  #      #   return (arr[0] + arr[1]) 
+  #       # else 
+  #        #  return result 
+
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+
+arr.combination(2) {|c| return true if c.sum ==n }
+false
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+'Hello, ' + name
+
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+/^[qwrtypsdfghjklzxcvbnm]/i.match(s)
+
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+/^(0|00|([01]+00))$/.match?(s)
+
+
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
-end
+attr_accessor :isbn, :price
+
+        def initialize isbn, price
+   raise ArgumentError if isbn.empty? || price <= 0
+                @isbn = isbn
+@price = price
+        end 
+    
+        def price_as_string
+                format("$%.2f", @price)
+        end 
+           
+end 
